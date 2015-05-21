673140484b01b3c03099ba9e1a870c6c2918a4a4
Nathan Hadinata <nathanhadinata@gmail.com>
2015-05-20 21:04:39 -0700

    add bio with commas this time

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,3 +6,15 @@ var formattedRole =
 
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedRole);
+
+var skills =
+["UX/UI Design", "Google Adwords", "Adobe Creative Suite"]
+
+var bio = {
+	"name" : "Nathan",
+	"role" : UX Designer,
+	"contact info" : nathanhadinata@gmail.com,
+	"picture URL" : "Nathan-Profile-150x150.jpg",
+	"welcome message" : "Boom boom boom, a lemme hear you say wayo",
+	"skills" : skills
+};
