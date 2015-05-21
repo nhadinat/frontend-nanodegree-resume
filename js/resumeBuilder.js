533ba30e98f8cb477e5c3ea9e04b041e33f29533
Nathan Hadinata <nathanhadinata@gmail.com>
2015-05-20 20:38:40 -0700

    add namechanger function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,3 +6,12 @@ var formattedRole =
 
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedRole);
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    var names = oldName.split(" ");
+    names[1] = names[1].toUpperCase();
+    names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+    finalName = names.join(" ");
+    return finalName;
+}
